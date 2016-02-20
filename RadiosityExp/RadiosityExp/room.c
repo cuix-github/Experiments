45dce353b24b6ed8ef7cc3da82e876864a3eef3b
Shakebones <residues.pheromones@gmail.com>
2016-02-20 11:41:13 -0330

    Wavelet turbulence added

--- RadiosityExp/RadiosityExp/room.c
+++ RadiosityExp/RadiosityExp/room.c
@@ -30,14 +30,14 @@ typedef struct {
 
 /* input parameters */
 TRadParams	params = {
-	0.0000001,			/* convergence threshold */
+	0.00000001,			/* convergence threshold */
 	0, 0, 0, 0, 0, 0,	/* patches, elements and points; initialize these in InitParams */
-	{{ 108, 120, 400 },	/* camera location */
+	{{ 108, 120, 600 },	/* camera location */
 	{ 108, 100, 100 },	/* look at point */
 	{ 0, 1, 0 },	/* up vector */
 	60, 60,			/* field of view in x, y*/
-	1, 550,			/* near, far */
-	800, 600,		/* resolution x, y */
+	1, 1000,			/* near, far */
+	1280, 720,		/* resolution x, y */
 	0 },			/* buffer */
 	100,			/* hemi-cube resolution */
 	250,			/* approximate diameter of the room */
@@ -106,25 +106,25 @@ static TSpectra black = {{ 0.0, 0.0, 0.0 }};
 /* Polygon vertices follow counter-clockwise order when viewing from front */
 #define numberOfPolys 	19
 TQuad roomPolys[numberOfPolys] = {
-	{{4, 5, 6, 7}, 4, 8, 75*75, {0, -1, 0}, &lightGrey, &black},
-	{{0, 3, 2, 1}, 4, 8, 75*75, {0, 1, 0}, &lightGrey, &black},
-	{{0, 4, 7, 3}, 4, 8, 75*75, {1, 0, 0}, &red, &black},
-	{{0, 1, 5, 4}, 4, 8, 75*75, {0, 0, 1}, &lightGrey, &black},
-	{{2, 6, 5, 1}, 4, 8, 75*75, {-1, 0, 0}, &blue, &black},
-	{{2, 3, 7, 6}, 4, 8, 75*75, {0, 0,-1}, &lightGrey, &black},
-	{{8, 9, 10, 11}, 4, 8, 75*75, {0, -1, 0}, &black, &white},
-	{{16, 19, 18, 17}, 4, 8, 75*75, {0, 1, 0}, &yellow, &black},
-	{{12, 13, 14, 15}, 4, 8, 75*75, {0, -1, 0}, &yellow, &black},
-	{{12, 15, 19, 16}, 4, 8, 75*75, {-0.866, 0, -0.5}, &yellow, &black},
-	{{12, 16, 17, 13}, 4, 8, 75*75, {0.5, 0, -0.866}, &yellow, &black},
-	{{14, 13, 17, 18}, 4, 8, 75*75, {0.866, 0, 0.5}, &yellow, &black},
-	{{14, 18, 19, 15}, 4, 8, 75*75, {-0.5, 0, 0.866}, &yellow, &black},
-	{{24, 27, 26, 25}, 4, 8, 75*75, {0, 1, 0}, &lightGrey, &black},
-	{{20, 21, 22, 23}, 4, 8, 75*75, {0, -1, 0}, &lightGrey, &black},
-	{{20, 23, 27, 24}, 4, 8, 75*75, {-0.866, 0, -0.5}, &lightGrey, &black},
-	{{20, 24, 25, 21}, 4, 8, 75*75, {0.5, 0, -0.866}, &lightGrey, &black},
-	{{22, 21, 25, 26}, 4, 8, 75*75, {0.866, 0, 0.5}, &lightGrey, &black},
-	{{22, 26, 27, 23}, 4, 8, 75*75, {-0.5, 0, 0.866}, &lightGrey, &black},
+	{ { 4, 5, 6, 7 }, 10, 5, 216 * 220, { 0, -1, 0 }, &lightGrey, &black }, // ceiling
+	{ { 0, 3, 2, 1 }, 10, 5, 216 * 220, { 0, 1, 0 }, &lightGrey, &black }, // floor
+	{ { 0, 4, 7, 3 }, 10, 5, 216 * 220, { 1, 0, 0 }, &red, &black }, // left wall
+	{ { 0, 1, 5, 4 }, 10, 5, 216 * 220, { 0, 0, 1 }, &lightGrey, &black }, // back wall
+	{ { 2, 6, 5, 1 }, 10, 5, 216 * 220, { -1, 0, 0 }, &blue, &black }, // right wall
+	{ { 2, 3, 7, 6 }, 10, 5, 216 * 220, { 0, 0, -1 }, &lightGrey, &black }, // ADDED front wall
+	{ { 8, 9, 10, 11 }, 8, 3, 55 * 55, { 0, -1, 0 }, &black, &white }, // light
+	{ { 16, 19, 18, 17 }, 10, 5, 60 * 50, { 0, 1, 0 }, &yellow, &black }, // box 1
+	{ { 12, 13, 14, 15 }, 10, 5, 60 * 50, { 0, -1, 0 }, &yellow, &black },
+	{ { 12, 15, 19, 16 }, 10, 5, 60 * 50, { -0.866, 0, -0.5 }, &yellow, &black },
+	{ { 12, 16, 17, 13 }, 10, 5, 60 * 50, { 0.5, 0, -0.866 }, &yellow, &black },
+	{ { 14, 13, 17, 18 }, 10, 5, 60 * 50, { 0.866, 0, 0.5 }, &yellow, &black },
+	{ { 14, 18, 19, 15 }, 10, 5, 60 * 50, { -0.5, 0, 0.866 }, &yellow, &black },
+	{ { 24, 27, 26, 25 }, 10, 5, 60 * 75, { 0, 1, 0 }, &lightGrey, &black }, // box 2
+	{ { 20, 21, 22, 23 }, 10, 5, 60 * 75, { 0, -1, 0 }, &lightGrey, &black },
+	{ { 20, 23, 27, 24 }, 10, 5, 60 * 75, { -0.866, 0, -0.5 }, &lightGrey, &black },
+	{ { 20, 24, 25, 21 }, 10, 5, 60 * 75, { 0.5, 0, -0.866 }, &lightGrey, &black },
+	{ { 22, 21, 25, 26 }, 10, 5, 60 * 75, { 0.866, 0, 0.5 }, &lightGrey, &black },
+	{ { 22, 26, 27, 23 }, 10, 5, 60 * 75, { -0.5, 0, 0.866 }, &lightGrey, &black },
 };
 
 /*
